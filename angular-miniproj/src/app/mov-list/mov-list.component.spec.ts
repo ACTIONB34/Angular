69e19608a14283e4030f6cae2853aa3ea7843246
IsabelleAWS <62867843+IsabelleAWS@users.noreply.github.com>
2020-06-04 15:28:43 +0800

    Create mov-list.component.spec.ts

---
+++ angular-miniproj/src/app/mov-list/mov-list.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { MovListComponent } from './mov-list.component';
+
+describe('MovListComponent', () => {
+  let component: MovListComponent;
+  let fixture: ComponentFixture<MovListComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ MovListComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(MovListComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
