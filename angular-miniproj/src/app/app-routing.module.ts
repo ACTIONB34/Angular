96e709e7e1c897aab7fd2ca114b4e64adaa91f0e
IsabelleAWS <62867843+IsabelleAWS@users.noreply.github.com>
2020-06-04 15:58:13 +0800

    Update app-routing.module.ts

--- angular-miniproj/src/app/app-routing.module.ts
+++ angular-miniproj/src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MovListComponent } from './mov-list/mov-list.component';
+import { ShowComponent } from './show/show.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'movies', component: MovListComponent, children: [
+    { path: '', redirectTo: 'info', pathMatch: 'full'},
+    { path: ':id', component: ShowComponent}
+  ]}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
